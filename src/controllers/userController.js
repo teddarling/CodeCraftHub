80c983e107245a0aabc94753201519235339473f
Ted Darling <ted@digitalspirittech.com>
2024-07-30 19:42:15 -0400

    WIP

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,6 +28,7 @@ export const registerUser = async (req, res) => {
 
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
+    console.log({error})
     res.status(500).json({ message: 'Server error' });
   }
 };
