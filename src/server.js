1c0ea003ff530ea8fd7e6f41698dcc62e6836acb
Ted Darling <ted@digitalspirittech.com>
2024-07-30 19:50:40 -0400

    WIP

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.log(err));
 
 // Use user routes
-app.use('/api/users', userRoutes);
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
